74dfff7cbb0fbfc7500d1e2b08e0c3cc7f933494
ramenha0141 <ramenhaoishii@gmail.com>
2024-07-16 23:01:57 +0900

    migrate to neon database

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,13 +1,19 @@
+import { Pool } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
+
+function createPrismaClient() {
+	const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+	const adapter = new PrismaNeon(neon);
+	const prisma = new PrismaClient({ adapter });
+
+	return prisma;
+}
 
 const globalForPrisma = global as unknown as {
 	prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-	globalForPrisma.prisma ??
-	new PrismaClient({
-		log: ['error'],
-	});
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
