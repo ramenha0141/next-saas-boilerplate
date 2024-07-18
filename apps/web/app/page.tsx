b81aebd2b871ed03a24165e222f3e5376205d94b
ramenha0141 <ramenhaoishii@gmail.com>
2024-07-18 13:17:03 +0900

    remove dark mode

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,11 @@
 import { auth, signIn } from '@repo/auth';
 import { Button } from '@repo/ui/shadcn/button';
 
-import { ThemeToggle } from './_components/theme-toggle';
-
 export default async function Home() {
 	const session = await auth();
 
 	return (
 		<div>
-			<ThemeToggle />
-
 			<form
 				action={async () => {
 					'use server';
